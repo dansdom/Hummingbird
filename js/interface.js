dbae5f23eef41d68a92f2666bd90c840c53858f7
Andy Coffey <andy@pollenizer.com>
2011-11-17 21:37:55 +1100

    Getting things humming
    Signed-off-by: Andy Coffey <andy@pollenizer.com>

---
+++ js/interface.js
@@ -0,0 +1,38 @@
+// custom javascript functions
+
+// this object to be called whatever the project name is
+Project = {
+	init : function()
+	{
+	}
+}
+
+$(document).ready(function()
+{
+	// initialise the Project object
+	Project.init();
+	// call plugins from here
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
