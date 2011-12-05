2b767b5363bf8fa1c20686086aac1e204f7b2d2a
Daniel Thomson <dansdom@gmail.com>
2011-12-05 14:59:05 +1100

    added background image and form validator

--- js/interface.js
+++ js/interface.js
@@ -4,8 +4,36 @@
 Project = {
 	init : function()
 	{
+		this.backgroundStretch();
+		this.validateLanding();
+	},
+	backgroundStretch : function()
+	{
+		$.backstretch("img/landing.jpg");
+	},
+	validateLanding : function()
+	{
+		$("#landingForm").validator({
+			inputTypes:{
+				usernameInput: {
+					inputName: 'username',
+					rules: {min:2,max:20},
+					error: 'min 2, max 20 chars allowed',
+					valid: 'this field is ok'
+				},
+				emailInput: {
+					inputName: 'email',
+					rules: {validEmail:true},
+					error: 'email not valid',
+					valid: 'this field is ok'
+				}
+			},
+			formClasses : { validClass : "help" },
+			onChangeValidation: false
+		});
 	}
-}
+};
+
 
 $(document).ready(function()
 {
